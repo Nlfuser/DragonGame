09b2e39cc42395958350df44eec0820c47ae3f66
MickailH <mh9g18@soton.ac.uk>
2024-02-02 12:27:27 +0000

    Gird instantiation and start of player instantiation. Added game manager to main scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int _width = 4;
+    [SerializeField] private int _height = 4;
+    [SerializeField] private Node _nodePrefab;
+
+
+    private List<Node> _nodes;
+    // private List<Block> _blocks;
+    private GameState _state;
+    private int _round;
+    // Start is called before the first frame update
+
+    void Start() {
+       ChangeState(GameState.GenerateLevel);
+    }
+
+    private void ChangeState(GameState newState) {
+        _state = newState;
+
+        switch (newState) {
+            case GameState.GenerateLevel:
+                GenerateGrid();
+                break;
+            // case GameState.SpawningBlocks:
+            //     SpawnBlocks(_round++ == 0 ? 2 : 1);
+                // break;
+            case GameState.WaitingInput:
+                break;
+            case GameState.Moving:
+                break;
+            // case GameState.Win:
+            //     _winScreen.SetActive(true);
+            //     Invoke(nameof(DelayedWinScreenText),1.5f);
+            //     break;
+            // case GameState.Lose:
+            //     _loseScreen.SetActive(true);
+                // break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+    }
+
+    
+    void Update() {
+        if(_state != GameState.WaitingInput) return;
+
+        // if(Input.GetKeyDown(KeyCode.LeftArrow)) Shift(Vector2.left);
+        // if(Input.GetKeyDown(KeyCode.RightArrow)) Shift(Vector2.right);
+        // if(Input.GetKeyDown(KeyCode.UpArrow)) Shift(Vector2.up);
+        // if(Input.GetKeyDown(KeyCode.DownArrow)) Shift(Vector2.down);
+    }
+
+    void GenerateGrid() {
+        _round = 0;
+        _nodes = new List<Node>();
+        // _blocks = new List<Block>();
+        for (int x = 0; x < _width; x++) {
+            for (int y = 0; y < _height; y++) {
+                var node = Instantiate(_nodePrefab, new Vector2(x, y), Quaternion.identity);
+                _nodes.Add(node);
+            }
+        }
+
+        var center = new Vector2((float) _width /2 - 0.5f,(float) _height / 2 -0.5f);
+
+        // var board = Instantiate(_boardPrefab, center, Quaternion.identity);
+        // board.size = new Vector2(_width,_height);
+
+        Camera.main.transform.position = new Vector3(center.x,center.y,-10);
+
+        ChangeState(GameState.SpawningBlocks);
+    }
+}
+
+public enum GameState {
+    GenerateLevel,
+    SpawningBlocks,
+    WaitingInput,
+    Moving,
+    Win,
+    Lose
+}
